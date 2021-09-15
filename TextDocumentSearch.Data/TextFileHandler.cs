01ba1a5ff50d42f79526e08e3993832184f32aa4
Jared Whaley <jjw0008@auburn.edu>
2021-09-15 16:09:00 -0400

    - Added TextFileHandler class. - Implemented ReadFromFile static method.

---
+++ TextDocumentSearch.Data/TextFileHandler.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace TextDocumentSearch.Data
+{
+    /// <summary>
+    /// Implements all functionality involving the import and export of data to and from text files.
+    /// </summary>
+    class TextFileHandler
+    {
+        /// <summary>
+        /// Takes a specified <paramref name="filepath"/> and returns the text contained within that file.
+        /// </summary>
+        /// <param name="filepath"></param>
+        /// <returns>
+        /// The text files contents as a string.
+        /// </returns>
+        public static string ReadTextFromFile(string filepath)
+        {
+            StreamReader streamReader = File.OpenText(filepath);
+            string txt = null;
+            // var lineSeparator = new[] { Environment.NewLine };
+            // var lines = new string[] { };
+
+            try
+            {
+                txt = streamReader.ReadToEnd();
+                // lines = txt.Split(lineSeparator, StringSplitOptions.RemoveEmptyEntries);
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            finally
+            {
+                streamReader.Close();
+            }
+            return txt;
+        }
+    }
+}
