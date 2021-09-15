f771bd46464f07cc20be25a3bb8e7f25c433e6d6
Jared Whaley <jjw0008@auburn.edu>
2021-09-15 14:35:24 -0400

    - Renamed btnBrowse to browseButton. - Added keywordLabel. - Added keywordTextBox.

--- TextDocumentSearch.UI/MainForm.Designer.cs
+++ TextDocumentSearch.UI/MainForm.Designer.cs
@@ -28,33 +28,56 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.btnBrowse = new System.Windows.Forms.Button();
+            this.browseButton = new System.Windows.Forms.Button();
+            this.keywordLabel = new System.Windows.Forms.Label();
+            this.keywordTextBox = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
-            // btnBrowse
+            // browseButton
             // 
-            this.btnBrowse.Location = new System.Drawing.Point(622, 357);
-            this.btnBrowse.Name = "btnBrowse";
-            this.btnBrowse.Size = new System.Drawing.Size(75, 23);
-            this.btnBrowse.TabIndex = 0;
-            this.btnBrowse.Text = "Browse...";
-            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.browseButton.Location = new System.Drawing.Point(622, 357);
+            this.browseButton.Name = "browseButton";
+            this.browseButton.Size = new System.Drawing.Size(75, 23);
+            this.browseButton.TabIndex = 0;
+            this.browseButton.Text = "Browse...";
+            this.browseButton.UseVisualStyleBackColor = true;
+            // 
+            // keywordLabel
+            // 
+            this.keywordLabel.AutoSize = true;
+            this.keywordLabel.Location = new System.Drawing.Point(94, 93);
+            this.keywordLabel.Name = "keywordLabel";
+            this.keywordLabel.Size = new System.Drawing.Size(102, 13);
+            this.keywordLabel.TabIndex = 1;
+            this.keywordLabel.Text = "Keyword/Sentence:";
+            // 
+            // keywordTextBox
+            // 
+            this.keywordTextBox.Location = new System.Drawing.Point(245, 90);
+            this.keywordTextBox.Name = "keywordTextBox";
+            this.keywordTextBox.Size = new System.Drawing.Size(100, 20);
+            this.keywordTextBox.TabIndex = 2;
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.keywordTextBox);
+            this.Controls.Add(this.keywordLabel);
+            this.Controls.Add(this.browseButton);
             this.Name = "MainForm";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.Button browseButton;
+        private System.Windows.Forms.Label keywordLabel;
+        private System.Windows.Forms.TextBox keywordTextBox;
     }
 }
 
